66b34d503b1a240196885de1b3ff8dba5bb01e27
Benjamin Goldberg <ben@spothero.com>
2019-10-14 14:36:33 -0500

    WIP

---
+++ go/cmd/thrift/thrift.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spothero/idl-test/go/v2/pkg/fortune"
+)
+
+func main() {
+	fmt.Println("hello, thrift")
+	fmt.Println(fortune.Animal_MONKEY)
+}
--- go/go.mod
+++ go/go.mod
@@ -3,6 +3,7 @@ module github.com/spothero/idl-test/go/v2
 go 1.13
 
 require (
+	github.com/apache/thrift v0.12.0
 	github.com/golang/protobuf v1.3.2
 	google.golang.org/grpc v1.24.0
 )
