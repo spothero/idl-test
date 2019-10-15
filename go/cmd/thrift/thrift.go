26bd6852355016ddb89a2c8f72ae36d65e4ede94
Benjamin Goldberg <ben@spothero.com>
2019-10-15 07:09:11 -0500

    working builds

--- go/cmd/thrift/thrift.go
+++ go/cmd/thrift/thrift.go
@@ -1,12 +1,47 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/apache/thrift/lib/go/thrift"
 	"github.com/spothero/idl-test/pkg/fortune"
+	"golang.org/x/xerrors"
 )
 
 func main() {
-	fmt.Println("hello, thrift")
-	fmt.Println(fortune.Animal_MONKEY)
+	if err := runServer(); err != nil {
+		panic(err)
+	}
+}
+
+func runServer() error {
+	fmt.Printf("starting thrift server at localhost:9090")
+	transport, err := thrift.NewTServerSocket("localhost:9090")
+	if err != nil {
+		return xerrors.Errorf("failed to open server socket: %w", err)
+	}
+
+	processor := fortune.NewFortuneTellerProcessor(FortuneTellerHandler{})
+	transportFactory := thrift.NewTTransportFactory()
+	protocolFactory := thrift.NewTCompactProtocolFactory()
+	server := thrift.NewTSimpleServer4(processor, transport, transportFactory, protocolFactory)
+	if err = server.Serve(); err != nil {
+		return xerrors.Errorf("failed to start server")
+	}
+	return nil
+}
+
+// Implementation of the FortuneTellerHandler
+type FortuneTellerHandler struct{}
+
+func (fth FortuneTellerHandler) GetFortune(
+	ctx context.Context,
+	fortune_request *fortune.FortuneRequest,
+) (r *fortune.FortuneResponse, err error) {
+	return &fortune.FortuneResponse{
+		Fortune:      "ricky, julian, and bubbles will not keep their riches",
+		LuckyNumbers: []int32{666},
+		LuckyAnimal:  fortune.Animal_CAT,
+	}, nil
 }
--- go/go.mod
+++ go/go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/apache/thrift v0.12.0
 	github.com/golang/protobuf v1.3.2
+	golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898 // indirect
 	google.golang.org/grpc v1.24.0
 )
