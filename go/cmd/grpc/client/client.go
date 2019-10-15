55140eb37109e43e1999becf0e16eeb72ccd96d6
Michael Wilson <wilsoniya@gmail.com>
2019-10-15 16:08:39 -0500

    golang-grpc client/server impl

--- go/cmd/grpc/client/client.go
+++ go/cmd/grpc/client/client.go
@@ -1,29 +1,61 @@
 package client
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+
 	"github.com/spothero/idl-test/pkg/grpcv1"
 )
 
 func NewCLICmd() *cobra.Command {
+	serverURL := ""
 	cmd := &cobra.Command{
 		Use:   "grpc-client",
 		Short: "Starts a GRPC Client",
-		Long:  "Starts a GRPC Client",
 		Run: func(cmd *cobra.Command, args []string) {
-			Run()
+			Run(serverURL)
 		},
 	}
 	flags := cmd.Flags()
-	serverURL := ""
 	flags.StringVar(&serverURL, "grpc-server-url", "localhost:9111", "GRPC Server URL")
 	return cmd
 }
 
-func Run() {
-	var test grpcv1.GetFortuneResponse_Animal
-	test = 1
-	fmt.Printf("The GRPC Client is not yet implemented, but here's an %v to tide you over\n", test)
+func Run(serverURL string) {
+	fmt.Printf("Sending request to %v\n", serverURL)
+	conn, err := grpc.Dial(serverURL, grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	client := grpcv1.NewFortuneTellerAPIClient(conn)
+
+	request := grpcv1.GetFortuneRequest{
+		Name: "Leeroy Jenkins",
+		OptionalCar: &grpcv1.GetFortuneRequest_Car{
+			Make:  "Geo",
+			Model: "Metro",
+			Year:  1992,
+		},
+		VehicleDescription: &grpcv1.GetFortuneRequest_BikeFrameSizeCm{BikeFrameSizeCm: 55},
+		FingerLengths:      []int32{32, 35, 900, 33, 29},
+		SiblingAges: map[string]int32{
+			"Greg":   17,
+			"Peter":  15,
+			"Bobby":  10,
+			"Cindy":  10,
+			"Marcia": 17,
+		},
+	}
+
+	response, err := client.GetFortune(context.Background(), &request)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Received response: %v\n", response)
 }
--- go/cmd/grpc/server/server.go
+++ go/cmd/grpc/server/server.go
@@ -1,29 +1,60 @@
 package server
 
 import (
+	"context"
 	"fmt"
+	"net"
 
 	"github.com/spf13/cobra"
 	"github.com/spothero/idl-test/pkg/grpcv1"
+	"google.golang.org/grpc"
 )
 
 func NewCLICmd() *cobra.Command {
+	serverURL := ""
 	cmd := &cobra.Command{
 		Use:   "grpc-server",
 		Short: "Starts a GRPC Server",
-		Long:  "Starts a GRPC Server",
 		Run: func(cmd *cobra.Command, args []string) {
-			Run()
+			Run(&serverURL)
 		},
 	}
 	flags := cmd.Flags()
-	serverURL := ""
 	flags.StringVar(&serverURL, "grpc-server-url", "localhost:9111", "GRPC Server URL")
 	return cmd
 }
 
-func Run() {
-	var test grpcv1.GetFortuneResponse_Animal
-	test = 1
-	fmt.Printf("The GRPC Server is not yet implemented, but here's an %v to tide you over\n", test)
+type fortuneTellerAPIImpl struct{}
+
+func (f fortuneTellerAPIImpl) GetFortune(
+	context context.Context,
+	request *grpcv1.GetFortuneRequest,
+) (*grpcv1.GetFortuneResponse, error) {
+
+	fmt.Printf("Received request: %v\n", *request)
+
+	response := grpcv1.GetFortuneResponse{
+		Fortune:      "A flute with no holes is not a flute and a donut with no holes is a Danish",
+		LuckyNumbers: []int32{420, 666},
+		LuckyAnimal:  grpcv1.GetFortuneResponse_ANIMAL_LIZARD,
+	}
+
+	return &response, nil
+}
+
+func Run(listenAddress *string) {
+	server := grpc.NewServer()
+
+	listener, err := net.Listen("tcp", *listenAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	grpcv1.RegisterFortuneTellerAPIServer(server, &fortuneTellerAPIImpl{})
+
+	fmt.Printf("Listening on %v ...\n", *listenAddress)
+	err = server.Serve(listener)
+	if err != nil {
+		panic(err)
+	}
 }
